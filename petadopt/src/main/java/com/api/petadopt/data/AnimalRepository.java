4b6d719fcd6fc53ea3a4c24c1bafa9ac25606562
Vanessa Rosa <vanessarosadevops@gmail.com>
2025-06-22 19:19:17 -0300

    Criação da classe AnimalRepository

--- petadopt/src/main/java/com/api/petadopt/data/AnimalRepository.java
+++ petadopt/src/main/java/com/api/petadopt/data/AnimalRepository.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 
 package com.api.petadopt.data;
@@ -9,6 +9,19 @@ package com.api.petadopt.data;
  *
  * @author vanes
  */
-public class AnimalRepository {
+import org.springframework.data.jpa.repository.JpaRepository; 
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+import com.api.petadopt.data.AnimalEntity;
+public interface AnimalRepository extends JpaRepository<AnimalEntity, Integer> {
+    Optional<AnimalEntity> findById(Integer id); 
+    Optional<AnimalEntity> findByNome(String nome);
+    List<AnimalEntity> findByEspecie(String especie);
+    List<AnimalEntity> findByGenero(String genero);
+    List<AnimalEntity> findByRaca(String raca);
+    List<AnimalEntity> findByPorte(String porte);
+    List<AnimalEntity> findByCor(String cor);
+    List<AnimalEntity> findByTemperamento(String temperamento);
 
 }
